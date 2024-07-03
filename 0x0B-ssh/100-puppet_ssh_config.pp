8713f27babb97991f54cdd9bcd6e8ac87d0c6a05
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-07-03 16:47:55 -0700

    Use puppet to add a new public key to server.

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,16 @@
-#Client configuration file (w/ Puppet)
-file { '/etc/ssh/ssh_config':
-  ensure  => present,
-  content => "
+# Seting up my client config file
+include stdlib
 
-    Host*
-    	IdentifyFile ~/.ssh/school
-    	PaaswordAuthentication no
-  ",
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'Delare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
