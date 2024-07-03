bbea48a6aeb21d2731063207507010e46fcceb36
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-07-03 06:27:07 -0700

    Install package using puppet.

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,14 @@
-#Install a package
-class { 'python3': }
+# Ensure Python 3 and pip3 are installed
+package { 'python3':
+  ensure => installed,
+}
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Install Flask using pip3 with a specific version
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-}
-package { 'Werkzeug':
-  ensure   => '2.1.1',
-  provider => 'pip3',
-}
-exec { 'python-installed':
-  command => '/usr/bin/which python3',
 }
