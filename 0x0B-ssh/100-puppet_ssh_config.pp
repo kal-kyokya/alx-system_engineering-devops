b175a95302abae272d5fc34b79e13470064927c5
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-07-03 16:34:38 -0700

    Use puppet to add a new public key to server.

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,1 +1,10 @@
-p
+#Client configuration file (w/ Puppet)
+file { '/etc/ssh/ssh_config':
+  ensure  => present,
+  content => "
+
+    Host*
+    	IdentifyFile ~/.ssh/school
+    	PaaswordAuthentication no
+  ",
+}
