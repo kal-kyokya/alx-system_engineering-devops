0da2d34c894cc1facf92bde5e55cbb03dd972746
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-07-03 06:15:21 -0700

    Install Flask using puppet.

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,13 @@
-# install flask from pip3
-package { 'flask' :
-    ensure   => '2.1.0',
-    provider => 'pip3',
-    }
+#Install a package
+class { 'python3': }
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
+exec { 'python-installed':
+  command => '/usr/bin/which python3',
+}
